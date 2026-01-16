4628ccf969b8363d143b704fc0db2aa0782d5e7b
Javohir <javohirrahimjonov546@gmail.com>
2026-01-16 13:58:19 +0300

    Smth end

---
+++ js/app.js
@@ -0,0 +1,230 @@
+let elContainer = document.getElementById("container");
+let elTemlate = document.getElementById("templateCard");
+let elTemlateSkeleton = document.getElementById("templateSkeleton");
+let elLoader = document.getElementById("loader");
+let elDelete = document.getElementById("deleteCard");
+let elPrev = document.getElementById("prev");
+let elNext = document.getElementById("next");
+let elPagenation = document.getElementById("pagenation");
+let elSubmit = document.getElementById("addSubmit");
+let elToastTemplate = document.getElementById("toastTemplate");
+let elToast = document.getElementById("toast");
+let elCarAddForm = document.getElementById("carAddForm");
+let elCarEditForm = document.getElementById("carEditForm");
+let elCarEditModal = document.getElementById("editModal");
+
+let limit = 6;
+let skip = 0;
+let = loader(true);
+function req() {
+  fetch(
+    `https://json-api.uz/api/project/fn44-amaliyot/cars?limit=${limit}&skip=${skip}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      ui(res.data);
+    })
+    .catch(() => {})
+    .finally(() => {
+      loader(false);
+    });
+}
+function loader(boolean) {
+  elLoader.innerHTML = null;
+
+  if (boolean) {
+    Array.from({ length: 6 }, (_, index) => index).forEach(() => {
+      elLoader.appendChild(elTemlateSkeleton.cloneNode(true).content);
+    });
+  }
+}
+function ui(data, clr) {
+  elPagenation.style.display = "flex";
+  if (skip === 0) {
+    elPrev.style.display = "none";
+  } else if (skip != 0) {
+    elPrev.style.display = "inline-block";
+  }
+  elContainer.innerHTML = "";
+  if (clr) {
+    elContainer.innerHTML = "";
+  }
+  data.forEach((element) => {
+    const clone = elTemlate.cloneNode(true).content;
+
+    clone.querySelector("h2").innerText = element.name
+      ? element.name
+      : "no data";
+
+    clone.querySelector("p").innerText = element.trim
+      ? element.trim
+      : "no data";
+    clone.getElementById("generation").innerText = element.generation
+      ? element.generation
+      : "no data";
+    clone.getElementById("year").innerText = element.year
+      ? element.year
+      : "no data";
+    clone.querySelector(".js-delete-button").id = element.id;
+    clone.querySelector(".js-edit-button").id = element.id;
+    clone.querySelector(".js-data-link").href =
+      location.origin + "/information.html?id=" + element.id;
+    elContainer.appendChild(clone);
+  });
+}
+
+// Delete section
+
+elContainer.addEventListener("click", (evt) => {
+  // Delete
+  if (evt.target.classList.contains("js-delete-button")) {
+    evt.target.disabled = true;
+    evt.target.innerHTML =
+      '<svg class="animate-spin color-white" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.90321 7.29677C1.90321 10.341 4.11041 12.4147 6.58893 12.8439C6.87255 12.893 7.06266 13.1627 7.01355 13.4464C6.96444 13.73 6.69471 13.9201 6.41109 13.871C3.49942 13.3668 0.86084 10.9127 0.86084 7.29677C0.860839 5.76009 1.55996 4.55245 2.37639 3.63377C2.96124 2.97568 3.63034 2.44135 4.16846 2.03202L2.53205 2.03202C2.25591 2.03202 2.03205 1.80816 2.03205 1.53202C2.03205 1.25588 2.25591 1.03202 2.53205 1.03202L5.53205 1.03202C5.80819 1.03202 6.03205 1.25588 6.03205 1.53202L6.03205 4.53202C6.03205 4.80816 5.80819 5.03202 5.53205 5.03202C5.25591 5.03202 5.03205 4.80816 5.03205 4.53202L5.03205 2.68645L5.03054 2.68759L5.03045 2.68766L5.03044 2.68767L5.03043 2.68767C4.45896 3.11868 3.76059 3.64538 3.15554 4.3262C2.44102 5.13021 1.90321 6.10154 1.90321 7.29677ZM13.0109 7.70321C13.0109 4.69115 10.8505 2.6296 8.40384 2.17029C8.12093 2.11718 7.93465 1.84479 7.98776 1.56188C8.04087 1.27898 8.31326 1.0927 8.59616 1.14581C11.4704 1.68541 14.0532 4.12605 14.0532 7.70321C14.0532 9.23988 13.3541 10.4475 12.5377 11.3662C11.9528 12.0243 11.2837 12.5586 10.7456 12.968L12.3821 12.968C12.6582 12.968 12.8821 13.1918 12.8821 13.468C12.8821 13.7441 12.6582 13.968 12.3821 13.968L9.38205 13.968C9.10591 13.968 8.88205 13.7441 8.88205 13.468L8.88205 10.468C8.88205 10.1918 9.10591 9.96796 9.38205 9.96796C9.65819 9.96796 9.88205 10.1918 9.88205 10.468L9.88205 12.3135L9.88362 12.3123C10.4551 11.8813 11.1535 11.3546 11.7585 10.6738C12.4731 9.86976 13.0109 8.89844 13.0109 7.70321Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>';
+    deleteCard(evt.target.id);
+  }
+  if (evt.target.classList.contains("js-edit-button")) {
+    evt.target.disabled = true;
+    evt.target.innerHTML =
+      '<svg class="animate-spin color-white" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.90321 7.29677C1.90321 10.341 4.11041 12.4147 6.58893 12.8439C6.87255 12.893 7.06266 13.1627 7.01355 13.4464C6.96444 13.73 6.69471 13.9201 6.41109 13.871C3.49942 13.3668 0.86084 10.9127 0.86084 7.29677C0.860839 5.76009 1.55996 4.55245 2.37639 3.63377C2.96124 2.97568 3.63034 2.44135 4.16846 2.03202L2.53205 2.03202C2.25591 2.03202 2.03205 1.80816 2.03205 1.53202C2.03205 1.25588 2.25591 1.03202 2.53205 1.03202L5.53205 1.03202C5.80819 1.03202 6.03205 1.25588 6.03205 1.53202L6.03205 4.53202C6.03205 4.80816 5.80819 5.03202 5.53205 5.03202C5.25591 5.03202 5.03205 4.80816 5.03205 4.53202L5.03205 2.68645L5.03054 2.68759L5.03045 2.68766L5.03044 2.68767L5.03043 2.68767C4.45896 3.11868 3.76059 3.64538 3.15554 4.3262C2.44102 5.13021 1.90321 6.10154 1.90321 7.29677ZM13.0109 7.70321C13.0109 4.69115 10.8505 2.6296 8.40384 2.17029C8.12093 2.11718 7.93465 1.84479 7.98776 1.56188C8.04087 1.27898 8.31326 1.0927 8.59616 1.14581C11.4704 1.68541 14.0532 4.12605 14.0532 7.70321C14.0532 9.23988 13.3541 10.4475 12.5377 11.3662C11.9528 12.0243 11.2837 12.5586 10.7456 12.968L12.3821 12.968C12.6582 12.968 12.8821 13.1918 12.8821 13.468C12.8821 13.7441 12.6582 13.968 12.3821 13.968L9.38205 13.968C9.10591 13.968 8.88205 13.7441 8.88205 13.468L8.88205 10.468C8.88205 10.1918 9.10591 9.96796 9.38205 9.96796C9.65819 9.96796 9.88205 10.1918 9.88205 10.468L9.88205 12.3135L9.88362 12.3123C10.4551 11.8813 11.1535 11.3546 11.7585 10.6738C12.4731 9.86976 13.0109 8.89844 13.0109 7.70321Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>';
+    getById(evt.target.id);
+  }
+});
+
+function deleteCard(id) {
+  fetch("https://json-api.uz/api/project/fn44-amaliyot/cars/" + id, {
+    method: "DELETE",
+  })
+    .then((res) => {
+      return res.text();
+    })
+    .then((res) => {
+      document.getElementById(id).closest(".card").remove();
+    })
+    .catch(() => {})
+    .finally(() => {});
+}
+// Edit
+function getById(id) {
+  fetch(`https://json-api.uz/api/project/fn44-amaliyot/cars/${id}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      elCarEditModal.showModal();
+      elCarEditForm.name.defaultValue = res.name;
+      elCarEditForm.year.defaultValue = res.year;
+      elCarEditForm.color.defaultValue = res.color;
+      elCarEditForm.maxSpeed.defaultValue = res.maxSpeed;
+      elCarEditForm.horsepower.defaultValue = res.horsepower;
+      elCarEditForm.generation.defaultValue = res.generation;
+      elCarEditForm.category.defaultValue = res.category;
+      elCarEditForm.doorCount.defaultValue = res.doorCount;
+      elCarEditForm.seatCount.defaultValue = res.seatCount;
+      elCarEditForm.acceleration.defaultValue = res.acceleration;
+      elCarEditForm.fuelType.defaultValue = res.fuelType;
+      elCarEditForm.country.defaultValue = res.country;
+      elCarEditForm.description.defaultValue = res.description;
+    })
+    .catch((res) => {})
+    .finally(() => {});
+}
+// pagenation
+elNext.addEventListener("click", (evt) => {
+  evt.target.disabled = true;
+  skip = skip + limit;
+
+  req();
+});
+elPrev.addEventListener("click", (evt) => {
+  evt.target.disabled = true;
+  skip = skip - limit;
+
+  req();
+});
+req();
+
+elCarAddForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const formData = new FormData(elCarAddForm);
+  let result = {};
+
+  formData.forEach((value, key) => {
+    result[key] = value;
+  });
+
+  for (let key in result) {
+    if (result[key] == "") {
+      elCarAddForm.childNodes.forEach((el) => {
+        if (el.classList != undefined && el.getAttribute("name") == key) {
+          el.focus();
+        }
+      });
+      const clone = elToastTemplate.cloneNode(true).content;
+      clone.querySelector("span").innerText = ` ${key} joyni to'ldiring`;
+      elToast.appendChild(clone);
+
+      setTimeout(() => {
+        document.querySelector(`[role="alert"]`).remove();
+      }, 1000);
+      return;
+    }
+  }
+  add(result);
+});
+
+// Post
+function add(data) {
+  fetch("https://json-api.uz/api/project/fn44-amaliyot/cars", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      elCarAddForm.reset();
+      ui([res], false);
+      document.getElementById("my_modal_3").close();
+    })
+    .catch(() => {})
+    .finally(() => {});
+}
+
+// document.getElementById("carEditForm").addEventListener("submit", (evt) => {
+//   evt.preventDefault();
+
+//   let x = [];
+
+//   document
+//     .getElementById("carEditForm")
+//     .querySelectorAll("input, textarea")
+//     .forEach((el) => {
+//       if (el.value.trim() == "") {
+//         x.push(el.name);
+//       }
+//     });
+//   console.log(x);
+
+//   if (x.length == 0) {
+//     let formData = new FormData(document.getElementById("carEditForm"));
+//     const reqObj = {
+//       name: formData.get("name"),
+//     };
+//     fetch("https://json-api.uz/api/project/fn44-amaliyot/cars/" + id, {
+//       method: "PATCH",
+//       headers: {
+//         "Content-Type": "applicaton/json",
+//       },
+//       body: JSON.stringify(reqObj),
+//     }).then((res) => {
+//       if (res.ok) {
+//         alert("Car succesfuly edited");
+//         req();
+//       }
+//     });
+//   }
+// });
